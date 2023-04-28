0c8ad402a965d1b80a4ce85000a38a46fadd6705
IvanIvanReg27 <makhorin0088@gmail.com>
2023-04-28 17:33:09 +0400

    commit

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -4,7 +4,13 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -12,16 +18,20 @@ public class Bot extends TelegramLongPollingBot {
     final private String BOT_NAME = "Natasha2000_bot";
 
     Storage storage;
+    ReplyKeyboard replyKeyboardMarkup;
 
     Bot() {
         storage = new Storage();
     }
+
     public String getBotUsername() {
         return BOT_NAME;
     }
+
     public String getBotToken() {
         return BOT_TOKEN;
     }
+
     public void onUpdateReceived(Update update) {
         try {
             if (update.hasMessage() && update.getMessage().hasText()) {
@@ -34,21 +44,38 @@ public class Bot extends TelegramLongPollingBot {
                 outMess.setText(response);
 
                 execute(outMess);
+                outMess.setReplyMarkup(replyKeyboardMarkup);
             }
         } catch (TelegramApiException e) {
             e.printStackTrace();
-            }
         }
-        public String parseMessage(String textMsg) {
+    }
+
+    public String parseMessage(String textMsg) {
         String response;
 
-        if(textMsg.equals("/start"))
-            response = "Привет, я рассказываю анекдоты. Всегда пиши только слово 'шутка', и тогда получишь новый анектод";
-        else if(textMsg.equals("шутка"))
+        if (textMsg.equals("/start"))
+            response = "Привет, я рассказываю анекдоты. Жми /get или напиши слово 'анекдот' и получишь новый анектод";
+        else if (textMsg.equals("/get") || textMsg.equals("анекдот"))
+
             //здесь старт ранее прописаной логиги (можно переписать)
             response = storage.getRandQuote();
         else
-            response = "Пиши только слово 'шутка', я еще в стадии разработки и поэтому не могу отвечать на другие запросы";
+            response = "Напиши слово 'анекдот'";
         return response;
-        }
     }
+
+    void initKeyboard() {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRows.add(keyboardRow);
+        keyboardRow.add(new KeyboardButton("анекдот"));
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+    }
+}
+
+
